87808af518b7a32fd3232c2069ff2cb3e8cda50b
EDA <eda@mac-mini-1.local>
2017-05-24 17:59:01 +1200

    last commit for day

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import request from 'superagent'
 
 export function getYoda (cb) {
   request
-     .get('https://yoda.p.mashape.com/yoda?sentence=you+will+learn')
+      // .get('https://yoda.p.mashape.com/yoda?sentence=you+will+learn')
+     .get('https://yoda.p.mashape.com/yoda')
      .set('X-Mashape-Key', 'ZMJzgwcAscmshawAmUX4cX74S9eRp1cNhTwjsne44SVA72j5LC')
      .set('Accept','text/plain')
    .end((err,res)=>{
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,28 +8,54 @@ class App extends React.Component {
       errMessage: null,
       yoda: ''
     }
+    // this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  componentDidMount () {
-    this.setState({errMessage: null})
-    getYoda((err, res) => {
+  handleSubmit (e) {
+    e.preventDefault()
+    this.setState({ errMessage: null})
+    getYoda((err, res)=>{
       if (err) {
-        this.setState({ errMessage: err })
+        this.setState({errMessage: err})
         return
       }
       this.setState({
         yoda: res
-      }, () => {
-        console.log('my state', this.state)
-      })
-
+      },() => {
+            console.log('my state', this.state)
     })
   }
+
+// handleChange (event){
+//   // console.log('this is state in handleChange function: ' + this.setState)
+// }
+  // componentDidMount () {
+  //   this.setState({errMessage: null})
+  //   getYoda((err, res) => {
+  //     if (err) {
+  //       this.setState({ errMessage: err })
+  //       return
+  //     }
+  //     this.setState({
+  //       yoda: res
+  //     }, () => {
+  //       console.log('my state', this.state)
+  //     })
+  //
+  //   })
+  // }
 
   render () {
   // console.log('Yodas message: '+ this.state.yoda)
     return (
       <div>
+        <h1>Yoda Speak</h1>
+        <p>Enter something to be yodified</p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} />
+          <button>Yodify</button>
+        </form>
         <p>{this.state.yoda}</p>
       </div>
     )
